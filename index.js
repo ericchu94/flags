8aac84df6cffbd5808617efd626f2332a9a533dd
Eric Chu <eric@ericchu.net>
2016-05-01 20:09:27 -0400

    Add views

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 'use strict';
 const Koa = require('koa');
 const co = require('co');
+const views = require('koa-views');
 
 const app = new Koa();
 
-app.use(ctx => {
-  ctx.body = 'Hi';
-});
+app.use(views(__dirname + '/views', {
+  map: {
+    ejs: 'ejs',
+  },
+  extension: 'ejs',
+}));
+
+app.use(co.wrap(function *(ctx) {
+  yield ctx.render('index');
+}));
 
 app.listen(3000);
