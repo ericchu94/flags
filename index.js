1d2dddb1617d2808fcd4d435606273588e932b3f
Eric Chu <eric@ericchu.net>
2016-05-02 01:00:07 -0400

    Add management

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Koa = require('koa');
 const co = require('co');
 const views = require('koa-views');
 const Router = require('koa-router');
+const serve = require('koa-static');
 
 const app = new Koa();
 const router = new Router();
@@ -14,23 +15,37 @@ app.use(views(__dirname + '/views', {
   extension: 'ejs',
 }));
 
+router.get('/assets/*', serve('.'));
+
 router.get('/', co.wrap(function *(ctx) {
   yield ctx.render('index', {
     flags: app.context.flags,
   });
 }));
 
-app.context.flags = [
-  {
-    name: 'flag1',
-  },
-  {
-    name: 'flag2',
-  },
-  {
-    name: 'flag3',
+router.get('/manage/:flag', co.wrap(function *(ctx) {
+  const flag = app.context.flags[ctx.params.flag];
+  if (flag) {
+    yield ctx.render('manage', {
+      flag: app.context.flags[ctx.params.flag],
+    });
   }
-];
+}));
+
+app.context.flags = {
+  flag1: {
+    name: 'flag1',
+    value: true,
+  },
+  flag2: {
+    name: 'flag2',
+    value: true,
+  },
+  flag3: {
+    name: 'flag3',
+    value: true,
+  },
+};
 
 app.use(router.routes());
 app.use(router.allowedMethods());
