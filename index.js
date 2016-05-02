f63e0febdcb7d04f4da499e7985714e4ce0b14d9
Eric Chu <eric@ericchu.net>
2016-05-01 20:39:13 -0400

    Add router

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const Koa = require('koa');
 const co = require('co');
 const views = require('koa-views');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
 
 app.use(views(__dirname + '/views', {
   map: {
@@ -12,8 +14,15 @@ app.use(views(__dirname + '/views', {
   extension: 'ejs',
 }));
 
-app.use(co.wrap(function *(ctx) {
-  yield ctx.render('index');
+router.get('/', co.wrap(function *(ctx) {
+  yield ctx.render('index', {
+    flags: app.context.flags,
+  });
 }));
+
+app.context.flags = [1, 2, 3];
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(3000);
