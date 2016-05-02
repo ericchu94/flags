ee83d01730025da053be0d69c920150a085c8fcb
Eric Chu <eric@ericchu.net>
2016-05-02 01:13:53 -0400

    Add saving

--- assets/manage.js
+++ assets/manage.js
@@ -1,3 +1,15 @@
 $(function () {
   $('#value').bootstrapSwitch();
+
+  $('#save').on('click', function () {
+    var checked = $('#value').is(':checked');
+    $.ajax('/flag/' + $('#name').text(), {
+      method: 'PUT',
+      data: checked,
+    }).then(function () {
+      window.location = '/';
+    }, function () {
+      alert('Failed!');
+    });
+  });
 });
