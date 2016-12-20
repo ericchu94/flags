dcfe3b44b63cfcb3716dc754107564e3391a3fc4
Eric Chu <eric@ericchu.net>
2016-12-20 10:45:12 -0800

    Switch to mysql

--- sequelize.js
+++ sequelize.js
@@ -1,9 +1,11 @@
 'use strict';
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('flagsd', null, null, {
-  dialect: 'sqlite',
-  storage: 'flags.sqlite',
+const sequelize = new Sequelize('flags', 'flags', 'flags', {
+  dialect: 'mysql',
+  dialectOptions: {
+    socketPath: '/var/run/mysqld/mysqld.sock',
+  },
 });
 
 exports.Sequelize = Sequelize;
